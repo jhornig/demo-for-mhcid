8ed9d1c279a0e5370a94f42d9712aa49fd1693d2
Joseph Hornig <49138500+jhornig@users.noreply.github.com>
2019-04-14 12:45:47 -0700

    Update index.php
    For assignment in Module 3

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-<?php header( 'Location: /index.html' ) ;  ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Demo for MHCID</title>
+  </head>
+  <body>
+    <h1>Demo for MHCID</h1>
+    <p>This is a sample web page that links Github to Heroku.</p>
+    <h4>Joseph Hornig</h4>
+  </body>
+</html>
+  
